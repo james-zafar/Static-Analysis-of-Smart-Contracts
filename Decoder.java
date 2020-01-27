53369ae1b6a99522a159fbe05dbef718d078dce9
James Zafar <JZafar1@users.noreply.github.com>
2020-01-27 10:06:17 +0000

    Improved readability of Decoder.java

--- Decoder.java
+++ Decoder.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 public class Decoder {
 
@@ -5,28 +6,33 @@ public class Decoder {
     private String address = "0xE0e9794A17aa5166c164b80fA0b126c72E5412B0";
     private String bytecode = "0x68466bad7e2343211320d5dcc03764c0ba522ad7aa22a9076d94f7a7519121dcaabbss1122ab01";
     //private String bytecode = "0x608060405234801561001057600080fd5b506040516020806103ee833981018060405281019080805190602001909291905050508060008190555080600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002081905550506103608061008e6000396000f300608060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806318160ddd1461005c57806370a0823114610087578063a9059cbb146100de575b600080fd5b34801561006857600080fd5b50610071610143565b6040518082815260200191505060405180910390f35b34801561009357600080fd5b506100c8600480360381019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919050505061014c565b6040518082815260200191505060405180910390f35b3480156100ea57600080fd5b50610129600480360381019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190505050610195565b604051808215151515815260200191505060405180910390f35b60008054905090565b6000600160008373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020549050919050565b60008073ffffffffffffffffffffffffffffffffffffffff168373ffffffffffffffffffffffffffffffffffffffff16141515156101d257600080fd5b600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054821115151561022057600080fd5b81600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000205403600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000208190555081600160008573ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000205401600160008573ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000208190555060019050929150505600a165627a7a7230582025608b5c372888ca7316c16f1748775824bc03cb64852867e119a10a03402ef500290000000000000000000000000000000000000000000000000000000000000000";
-    private List<String> theOpcodes = new ArrayList<String>();
+    private List<String> theOpcodes;
     //HashMap contains [Code, Name], [added to stack, removed from stack]
-    private HashMap<List<String>, List<String>> decoded = new HashMap<List<String>, List<String>>();
-    public final List<Opcode> allOpcodes = list.getList();
-    private Opcode myCode = new Opcode();
-    public Stack stack = new Stack();
-    public Stack memory = new Stack();
-    private StopAndArithmetic completeArithmeticOps = new StopAndArithmetic();
+    private HashMap<List<String>, List<String>> decoded;
+    public final List<Opcode> allOpcodes;
+    public Stack stack, memory;
+    private StopAndArithmetic completeArithmeticOps;
 
     public Decoder() {
+        decoded = new HashMap<List<String>, List<String>>();
+        FormatBytecode formatter = new FormatBytecode(decoded);
+        List<String> finished = new ArrayList<String>();
+        finished = formatter.getFormattedData();
+        finished.forEach(current -> System.out.print(current + "\n"));
+        theOpcodes = new ArrayList<String>();
+        stack = new Stack();
+        memory = new Stack();
+        allOpcodes = list.getList();
+        completeArithmeticOps = new StopAndArithmetic();
+
         //Remove leading 0x in any input bytecode
         if(bytecode.startsWith("0x")) {
-            bytecode = bytecode.substring(2, bytecode.length());
+            bytecode = bytecode.substring(2);
         }
         splitCode();
         //System.out.println(Arrays.toString(theOpcodes.toArray()));
         decodeBytecode();
         //System.out.println(Arrays.asList(decoded));
-        FormatBytecode formatter = new FormatBytecode(decoded);
-        List<String> finished = new ArrayList<String>();
-        finished = formatter.getFormattedData();
-        finished.forEach(current -> System.out.print(current + "\n"));
     }
 
     private void splitCode() {
@@ -41,7 +47,7 @@ public class Decoder {
     }
 
     private void decodeBytecode() {
-        List<String> additionalData = new ArrayList<String>();
+        List<String> additionalData;
         while(theOpcodes.size() > 0) {
             List<String> tempList = new ArrayList<String>();
             String opcodeName = findOpcodeName(theOpcodes.get(0));
@@ -62,7 +68,6 @@ public class Decoder {
             int counter = 0;
             while (counter < allOpcodes.size()) {
                 if (allOpcodes.get(counter).getCode().matches(opcode)) {
-                    myCode = allOpcodes.get(counter);
                     return allOpcodes.get(counter).getName();
                 }
                 counter++;
@@ -78,13 +83,13 @@ public class Decoder {
         List<String> stackAmendments = new ArrayList<String>();
         int counter = 0;
         int noBytes = Character.digit((theCode.charAt(theCode.length() - 1)), 16);
-        char swtichChar = theCode.charAt(0);
-        switch(swtichChar) {
+        char switchChar = theCode.charAt(0);
+        switch(switchChar) {
             case '0':
                 stackAmendments = arithmeticOp(theCode);
                 break;
             case '1':
-                //Comparsion Operations
+                //Comparison Operations
                 stackAmendments = doComparison(theCode);
                 break;
             case '2':
@@ -116,9 +121,9 @@ public class Decoder {
             case '6':
             case '7':
                 //Push Operations
-                StringBuilder sb = new StringBuilder("");
+                StringBuilder sb = new StringBuilder();
                 while(counter <= noBytes) {
-                    sb.append(theOpcodes.get(counter) + " ");
+                    sb.append(theOpcodes.get(counter)).append(" ");
                     stack.push(theOpcodes.get(counter));
                     counter++;
                 }
@@ -166,13 +171,7 @@ public class Decoder {
             Object result = method.invoke(classRef, arg1, arg2);
             res = (String) result;
             stack.push(res);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch(IllegalAccessException e) {
-            e.printStackTrace();
-        } catch(java.lang.reflect.InvocationTargetException e) {
-            e.printStackTrace();
-        } catch(ClassNotFoundException e) {
+        } catch (NoSuchMethodException | ClassNotFoundException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
         additionalInfo.add(res);
@@ -214,13 +213,7 @@ public class Decoder {
             stack.push(res);
             additionalInfo.add(res);
             additionalInfo.add(removed);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch(IllegalAccessException e) {
-            e.printStackTrace();
-        } catch(java.lang.reflect.InvocationTargetException e) {
-            e.printStackTrace();
-        } catch(ClassNotFoundException e) {
+        } catch (NoSuchMethodException | ClassNotFoundException | InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
         return additionalInfo;
@@ -229,7 +222,7 @@ public class Decoder {
     private List<String> stackOperations(String theCode) {
         List<String> additionalInfo = new ArrayList<String>();
         //String methodToCall = completeArithmeticOps.getOpcodeName(theCode).toLowerCase();
-        String memWord, stackWord;
+        String memWord;
         if(theCode.matches("51")) {
             additionalInfo.add(memory.get(0));
             additionalInfo.add(stack.get(0));
@@ -252,13 +245,7 @@ public class Decoder {
             Class<?> myClass = Class.forName("StackMemory");
             java.lang.reflect.Method method = myClass.getDeclaredMethod(methodName);
             method.invoke(completeArithmeticOps);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch(IllegalAccessException e) {
-            e.printStackTrace();
-        } catch(java.lang.reflect.InvocationTargetException e) {
-            e.printStackTrace();
-        } catch(ClassNotFoundException e) {
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
