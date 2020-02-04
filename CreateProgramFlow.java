f4cf9632143daf0d63448fca0a1d17673fd15a5d
James Zafar <JZafar1@users.noreply.github.com>
2020-02-04 13:53:20 +0000

    Fixed issue with branches always being empty

--- CreateProgramFlow.java
+++ CreateProgramFlow.java
@@ -1,70 +1,79 @@
 import java.util.*;
 public class CreateProgramFlow {
-    public HashMap<Integer, List<String>> programFlow;
+    public HashMap<Integer, ArrayList<String>> programFlow;
     private List<String> finished;
     private List<String> opcodes;
-    private List<String> currentBranch;
     private List<Integer> index;
     int i, branchNumber = 1;
     public CreateProgramFlow(List<String> opcodes, List<String> finished) throws ProgramFlowException {
         this.finished = finished;
         this.opcodes = opcodes;
-        currentBranch = new ArrayList<String>();
-        programFlow = new HashMap<>();
+        ArrayList<String> currentBranch = new ArrayList<String>();
+        programFlow = new HashMap<Integer, ArrayList<String>>();
         index = new ArrayList<Integer>();
         for(i = 0; i < opcodes.size(); i++) {
+            currentBranch = new ArrayList<String>();
             index.add(i);
-            manageOpcode(this.opcodes.get(i));
+            manageOpcode(this.opcodes.get(i), currentBranch);
         }
-        for (Map.Entry<Integer, List<String>> entry : programFlow.entrySet()) {
+        if(!currentBranch.isEmpty()) {
+            addBranch(currentBranch);
+        }
+
+        for (Map.Entry<Integer, ArrayList<String>> entry : programFlow.entrySet()) {
+            ArrayList<String> temp1 = programFlow.get(1);
             String listString = String.join(", ", entry.getValue());
             System.out.println("Key = " + entry.getKey() + ", Value = " + listString);
         }
         //finished.forEach(current -> System.out.print(current + "\n"));
-        //System.out.println(Arrays.asList(programFlow));
         //System.out.println(Arrays.deepToString(programFlow.entrySet().toArray()));
 
     }
 
-    private void manageOpcode(String code) throws ProgramFlowException {
+    private void missedOpcodes(List<Integer> codes) {
+        List<Integer> missedOpcodes = new ArrayList<Integer>();
+
+    }
+
+    private void manageOpcode(String code, ArrayList<String> branch) throws ProgramFlowException {
         switch(code) {
             case "JUMP":
-                addBranch();
-                doJump();
+                addBranch(branch);
+                doJump(branch);
                 break;
             case "JUMPI":
-                doJumpI();
+                doJumpI(branch);
                 break;
             case "SELFDESTRUCT":
             case "STOP":
-                addKill();
+                addKill(branch);
                 break;
             default:
-                currentBranch.add(finished.get(i));
+                branch.add(finished.get(i));
         }
     }
 
-    private synchronized void addBranch() {
-        //String listString = String.join(", ", currentBranch);
-        //System.out.println("Current branch content: " + listString);
+    private synchronized void addBranch(ArrayList<String> currentBranch) {
+        if(programFlow.containsKey(branchNumber)) {
+            branchNumber++;
+        }
         programFlow.put(branchNumber, currentBranch);
-        currentBranch.clear();
         branchNumber++;
     }
 
-    private void doJump() throws ProgramFlowException {
+    private void doJump(ArrayList<String> currentBranch) throws ProgramFlowException {
         String temp = finished.get(i);
         List<String> split = new LinkedList<String>(Arrays.asList(temp.split("\\s+")));
         String jumpToLocation = split.get(4).replaceAll("\\D+", "");
         try {
             if (opcodes.get(Integer.parseInt(jumpToLocation)).matches("JUMPDEST")) {
                 //This marks a valid jump
-                i = Integer.parseInt(split.get(4), 16);
+                i = Integer.parseInt(split.get(4), 16) - 1;
                 split.set(3, "None");
                 split.remove(4);
                 currentBranch.add(split.toString());
             } else {
-                addFailedJump(split, "Jump conditions not met");
+                addFailedJump(split, "Jump conditions not met", currentBranch);
             }
         }catch(IndexOutOfBoundsException e) {
             try {
@@ -73,35 +82,35 @@ public class CreateProgramFlow {
                 ex.printStackTrace();
             }
         }finally {
-            addFailedJump(split, "The jump location does not exist");
+            addFailedJump(split, "The jump location does not exist", currentBranch);
             i++;
             index.add(i);
-            manageOpcode(this.opcodes.get(i));
+            manageOpcode(this.opcodes.get(i), currentBranch);
         }
     }
 
-    private void addFailedJump(List<String> split, String reason) {
+    private void addFailedJump(List<String> split, String reason, ArrayList<String> currentBranch) {
         String setReason = "Failed jump: " + reason;
         split.set(3, setReason);
         split.remove(4);
         currentBranch.add(split.toString());
-        addBranch();
+        addBranch(currentBranch);
     }
 
-    private void doJumpI() throws ProgramFlowException {
+    private void doJumpI(ArrayList<String> currentBranch) throws ProgramFlowException {
         String info = finished.get(i);
         if(info.contains("VALID")) {
-            doJump();
+            doJump(currentBranch);
         }else {
             List<String> split = Arrays.asList(info.split("//s+"));
             split.set(2, "None");
-            addBranch();
+            addBranch(currentBranch);
         }
     }
 
-    private void addKill() {
+    private void addKill(ArrayList<String> currentBranch) {
         currentBranch.add(finished.get(i));
-        addBranch();
+        addBranch(currentBranch);
     }
 
 }
