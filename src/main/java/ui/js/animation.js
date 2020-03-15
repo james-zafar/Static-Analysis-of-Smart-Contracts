0bca67f9ebbc9c23563c31d323a9c952e19dc1e1
James Zafar <JZafar1@users.noreply.github.com>
2020-03-15 21:04:13 +0000

    AWS callback now functional

--- src/main/java/ui/js/animation.js
+++ src/main/java/ui/js/animation.js
@@ -24,24 +24,21 @@ $(document).ready(function() {
      window.s3.getObject(params, function (error, data) {
          if (error) {
              console.log(error);
+             callback(error);
          } else {
              const fileContents = data.Body.toString();
-             var split = fileContents.split("\n");
-             window.inputNodes = split[0];
-             window.inputEdges = split[1];
-             console.log(window.inputNodes);
-             console.log(window.inputEdges);
+             window.split = fileContents.split("\n");
+             callback();
          }
      });
-     callback();
  }
 
 /**
  * Generate animated display using Cytoscape.js
  */
  function launchUI() {
-    console.log(window.inputNodes);
-    console.log(window.inputEdges);
+    console.log(window.split[0]);
+    console.log(window.split[1]);
     var cy = window.cy = cytoscape({
         container: document.getElementById('cy'),
 
@@ -63,12 +60,72 @@ $(document).ready(function() {
         ],
 
         elements: {
-            nodes: window.inputNodes,
-            edges: window.inputEdges
+            nodes: window.split[0],
+            edges: window.split[1]
+            /*
+            nodes: [
+                {data: {id: 'Branch1'}},
+                {data: {id: 'Branch2'}},
+                {data: {id: 'Branch3'}},
+                {data: {id: 'Branch4'}}
+            ],
+            edges: [
+                {data: {id: 'b12', source: 'Branch1', target: 'Branch2'}},
+                {data: {id: 'b13', source: 'Branch1', target: 'Branch3'}},
+                {data: {id: 'b23', source: 'Branch2', target: 'Branch3'}},
+                {data: {id: 'b24', source: 'Branch2', target: 'Branch4'}},
+                {data: {id: 'b14', source: 'Branch1', target: 'Branch4'}}
+            ]
+             */
         },
 
         layout: {
             name: 'breadthfirst'
         }
     });
+
+    /*var a = cy.getElementById('Branch1');
+    var b = cy.getElementById('Branch2');
+    var ab = cy.getElementById('b23');
+    var c = cy.getElementById('Branch3');
+    var d = cy.getElementById('Branch 4');
+
+    var makeDiv = function (text) {
+        var div = document.createElement('div');
+
+        div.classList.add('popper-div');
+
+        div.innerHTML = text;
+
+        document.body.appendChild(div);
+
+        return div;
+    };
+
+    var popperA = a.popper({
+        content: function () {
+            return makeDiv('Additional Info');
+        }
+    });
+
+    var updateA = function () {
+        popperA.scheduleUpdate();
+    };
+
+    a.on('position', updateA);
+    cy.on('pan zoom resize', updateA);
+
+
+    var popperAB = ab.popper({
+        content: function () {
+            return makeDiv('Branch not executed');
+        }
+    });
+
+    var updateAB = function () {
+        popperAB.scheduleUpdate();
+    };
+
+    ab.connectedNodes().on('position', updateAB);
+    cy.on('pan zoom resize', updateAB);*/
 }
